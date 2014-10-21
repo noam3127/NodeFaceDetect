6cf3e4e04bdfc542c35088eb5552cee5ea8d45b6
Noam Lustiger <noshev27@gmail.com>
2014-10-20 21:06:15 -0400

    minor fixes

--- faceDetect.js
+++ faceDetect.js
@@ -10,11 +10,10 @@ module.exports = function(path, imgName, socket) {
         im.ellipse(x.x + x.width / 2, x.y + x.height / 2, x.width / 2, x.height / 2);
         //var COLOR = [255, 0, 0];
         //im.rectangle([face.x, face.y], [face.x + face.width / 2, face.y + face.height / 2], COLOR, 2);
-	    }
+      }
 	    im.save('./public/detectedFaces/' + imgName);
       console.log(im);
       socket.emit('faceImage', {src: './detectedFaces/' + imgName});
-
-	  });
+    });
   });
 };
